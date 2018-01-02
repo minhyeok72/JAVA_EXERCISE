ce15b0a97854d9aaf09dce8725dc93b147c9bb51
admin <admin@10.137.138.151>
2018-01-02 18:14:29 +0900

    Chapter03

---
+++ src/icehs/science/chapter03/MyAgeTest.java
@@ -0,0 +1,17 @@
+package icehs.science.chapter03;
+
+public class MyAgeTest {
+
+	public static void main(String[] args) {
+		
+		int bornYear;
+		bornYear = 2000;
+		int age;
+		age = 19;
+		
+		System.out.println( bornYear );
+		System.out.println( age );
+
+	}
+
+}
---
+++ src/icehs/science/chapter03/Variable.java
@@ -0,0 +1,17 @@
+package icehs.science.chapter03;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int age = 19;
+		int korScore = 95;
+		int engScore = 100;
+		int mathScore = 88;
+		int totalGoodsPrice = 111111;
+		System.out.println(age);
+		System.out.print(age);
+		System.out.println(age);
+
+	}
+
+}
